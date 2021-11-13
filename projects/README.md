d92f43ae7aacff1f131a82a7ebb91744471bebda
anamariasosam <anamariasosam@gmail.com>
2021-11-12 20:32:30 -0500

    Modulo1: GuessWho

---
+++ projects/README.md
@@ -0,0 +1,7 @@
+# Proyectos
+
+Aquí encontrarás los proyectos que vamos realizando en nuestras clase
+
+## Módulo 1
+
+- [Guess Who](guess-who/)
---
+++ projects/guess-who/character.js
@@ -0,0 +1,18 @@
+class Character {
+  constructor(name, hairColor) {
+    this.name = name;
+    this.hairColor = hairColor;
+  }
+}
+
+const generatedCharacters = [
+  new Character("alex", "negro"), //0
+  new Character("alfred", "naranja"), //1
+  new Character("anita", "amarillo"),
+  new Character("anne", "negro"),
+  new Character("bernard", "cafe"),
+  new Character("david", "amarillo"),
+  new Character("eric", "amarillo"),
+  new Character("frans", "naranja"),
+  new Character("george", "blanco"), // 8
+];
