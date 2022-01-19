15cfe1c1b2c5170dc034873174a6281535bcbdfe
anamariasosam <anamariasosam@gmail.com>
2022-01-19 18:44:20 -0500

    Resultados electorales con navbar

--- projects/resultados-electorales/src/App.js
+++ projects/resultados-electorales/src/App.js
@@ -1,10 +1,26 @@
 import { Elecciones } from './pages/Elecciones';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { CandidatosProvider } from './utils/CandidatosContext';
-
+import { Navbar } from './components/NavBar';
+import { Box } from '@mui/material';
+import { RegistrarCandidato } from './pages/RegistrarCandidato';
+import './_App.css';
 function App() {
   return (
     <CandidatosProvider>
-      <Elecciones />
+      <BrowserRouter>
+        <Navbar />
+        <Box
+          sx={{
+            margin: 2,
+          }}
+        >
+          <Routes>
+            <Route index element={<Elecciones />}></Route>
+            <Route path="registrar" element={<RegistrarCandidato />} />
+          </Routes>
+        </Box>
+      </BrowserRouter>
     </CandidatosProvider>
   );
 }
